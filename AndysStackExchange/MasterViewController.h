b14c276e30346a9d29a00ef5c5e2323dcbed2306
obusek <obusek@gmail.com>
2013-10-07 21:08:04 -0400

    Added a comment

--- AndysStackExchange/MasterViewController.h
+++ AndysStackExchange/MasterViewController.h
@@ -9,6 +9,10 @@
 #import <UIKit/UIKit.h>
 #import "ItemsAPI.h"
 
+/**
+ The initial controller visible when viewing the application.  Shows a list of questions from stackexchange.com and
+ allows a user to select one to view details about the question.
+ */
 @interface MasterViewController : UIViewController <ItemsAPIDelegate> {
     NSArray *items;
 }
